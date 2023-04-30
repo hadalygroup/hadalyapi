b8b36d82f09badf307bc7a987ad30871b080932d
toumelin <atoumelin@yahoo.fr>
2023-04-30 17:58:06 -0400

    add input to /hitoric

--- main.py
+++ main.py
@@ -51,11 +51,12 @@ def current_user():
     }
 
 @app.get("/historic")
-async def historic():
+# /historic?symbol=aapl&start_date=2022-01-06&end_date=2022-01-12&interval=1d
+async def historic(symbol: str, start_date: str, end_date: str, interval: str):
     # for demonstration purposes, this is a slow endpoint that waits 5 seconds
     await asyncio.sleep(5)
     try:
-        res=historical_data_gmd("aapl", '2022-01-06', '2022-01-12', '1d')
+        res = historical_data_gmd(symbol, start_date, end_date, interval)
         for key, value in res.items():
             if isinstance(value, np.ndarray):
                 res[key] = value.tolist()
@@ -63,7 +64,7 @@ async def historic():
         # Convert the dictionary to a JSON-formatted string
         res = json.dumps(res)
     except Exception as e:
-        res='error :'+ str(e)
+        res = 'error :' + str(e)
     return res
 
 

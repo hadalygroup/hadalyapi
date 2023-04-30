11b91ae74d349dd07d7c1c1361069c43fbe52bed
toumelin <atoumelin@yahoo.fr>
2023-04-29 21:03:36 -0400

    update main

--- main.py
+++ main.py
@@ -51,11 +51,10 @@ def current_user():
 async def data():
     # for demonstration purposes, this is a slow endpoint that waits 5 seconds
     await asyncio.sleep(5)
-    # this endpoint's repsonse will match the UserResponse model
     try:
-        res=historical_data_gmd('AAPL',
+        res=historical_data_gmd('MSFT',
                             '01-01-2022',
-                            '02-02-2022',
+                            '03-02-2022',
                             '1d',  PRE_PERIOD=0)
     except Exception as e:
         res=e
